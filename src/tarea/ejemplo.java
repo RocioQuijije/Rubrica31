a2e8669248a8ad8ff49635c1fbbf7e2c51ecca1f
Rocio <Rocio@Roshy>
2016-08-25 22:06:28 -0500

    Actualizando proyecto

---
+++ src/tarea/ejemplo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+ import java.util.Random;
+
+/**
+ *
+ * @author Rocio
+ */
+public class ejemplo {
+   public static void main(String arg[]) {
+            int a, b, c;
+            Random rnd = new Random();
+            a = rnd.nextInt(101);
+            b = rnd.nextInt(101);
+            c = rnd.nextInt(101);      
+            System.out.println(a);        
+            System.out.println(b);        
+            System.out.println(c);        
+      }
+ }
+    
+     
+    
+ 
+    
+
